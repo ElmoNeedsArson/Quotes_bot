2c40e16b310c6e22ae4cb06d5ac810176db29dd1
ElmoNeedsArson <jesse.strijker@gmail.com>
2026-01-27 14:53:08 +0100

    textual fix progress embed

--- commands/progress.js
+++ commands/progress.js
@@ -42,7 +42,7 @@ module.exports = {
       .setColor('#00aa88')
       .setDescription(
         `${bar} **${percentText}%**\n\n` +
-        `**${voted}** / **${total}** quotes have been voted on` +
+        `**${voted}** / **${total}** quotes have been voted on\n` +
         `2 daily quotes means probably done in ${voted/2} days`
       )
       .setFooter({
